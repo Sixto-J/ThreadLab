28b820617f010b7c3171856d6a7cd7b107363497
Sixto-J <jvera@cifpfbmoll.eu>
2025-01-30 19:29:10 +0100

    botones start y stop añadidos

--- src/main/java/ConfigurationPanel.java
+++ src/main/java/ConfigurationPanel.java
@@ -1,8 +1,31 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class ConfigurationPanel {
 
+    public static void main(String[] args) {
+        // Create a new JButton
+        JButton button = new JButton("Play");
 
+        // Add an ActionListener to the button
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Action to be performed when the button is clicked
+            }
+        });
+        // Create a new JButton
+        JButton button2 = new JButton("Stop");
 
+        // Add an ActionListener to the button
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Action to be performed when the button is clicked
+            }
+        });
 
-
+    }
 
 }
